0cec0989a702f97301c4434b405715bfb64377f1
JoshEvans33 <evans@broadinstitute.org>
2025-08-29 16:07:26 -0400

    Configs Updated.

---
+++ run_once_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#########################################
+# Script for bootstraping fresh install #
+#########################################
+
+echo "Installing apps from Homebrew"
+brew bundle --global
